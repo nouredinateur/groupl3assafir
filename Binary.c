294c61f443f7ba47136d64a4f62837fdfeb0d062
fatimajd <jdiouifatimazahera@gmail.com>
2021-01-22 12:13:06 +0100

    new

--- Binary.c
+++ Binary.c
@@ -1,14 +1,38 @@
 #include <stdio.h>
 
-
-
+int intToBin(int x);
+int binToint();
 int  sumbinaire();
 
-int main(){
+int main()
+{
 	
 	
 }
 
-int  sumbinaire(int a , int b){
+int intToBin(int x)
+{
+	
+}
 
-	}
+int  sumbinaire(int a , int b){
+		int s;
+		if(a==0 && b==1){
+			s=1;
+		}else 	if(a==1 && b==0){
+			s=1;
+		}else 	if(a==0 && b==0){
+			s=0;
+		}else 	if(a==1 && b==1){
+			s=10;
+		}
+		return s;
+}
+
+
+int binToint(){
+	
+	
+	
+}
+
