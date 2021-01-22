c0975fd70a70d60cf001396ebbe999e31f8b38a3
fatimajd <jdiouifatimazahera@gmail.com>
2021-01-22 14:54:57 +0100

    new

--- Binary.c
+++ Binary.c
@@ -7,7 +7,7 @@ int  sumbinaire();
 int main()
 {
 	
-	
+ sumbinaire();
 }
 
 int intToBin(int x)
@@ -18,9 +18,11 @@ int intToBin(int x)
 int sumbinaire(int A[4], int B[4], int s[4])
 {
     int i;
+   
  
     for(i=0; i<4; i++)
     {
+    	
         if((A[i] == '0') && (B[i] == '0'))
             s[i] = '0';
         if((A[i] == '0') && (B[i] == '1'))
@@ -30,9 +32,12 @@ int sumbinaire(int A[4], int B[4], int s[4])
         if((A[i] == '1') && (B[i] == '1'))
             s[i] = '1';
     }
+     printf("doner\n");
+    scanf("%d\n",A);
+    printf("doner\n");
+    scanf("%d\n",B);
     return s[i];
 }
-
 
 int binToint(){
 	
