879b5797e8039764e6f86dfd3f32971b2c71fb02
amine <saissihassaniamine@gmail.com>
2021-01-22 12:13:38 +0100

    intToBin function

--- Binary.c
+++ Binary.c
@@ -1,31 +1,45 @@
 #include <stdio.h>
 
-
 void intToBin(int x);
-
+int binToint();
+int  sumbinaire();
 
 int main()
 {
-	return 0;
+	
+	
 }
 
 void intToBin(int x)
 {
-	if (x < 0 || x > 15)
-	{
-		printf("the input should be greater or equal 0 and less than 16\n");
-	}
-	int bin[4];
-	int temp = x;
-	for (int i = 0; i < 4; i++)
-	{
-		bin[i] = temp % 2;
-		temp = temp / 2;
-	}
-	
-	for (int i = 3; i >= 0; i--)
-	{
-		printf("%i", bin[i]);
-	}
-	printf("\n");
+    if (x < 0 || x > 15)
+    {
+        printf("the input should be greater or equal 0 and less than 16\n");
+    }
+    int bin[4];
+    int temp = x;
+    for (int i = 0; i < 4; i++)
+    {
+        bin[i] = temp % 2;
+        temp = temp / 2;
+    }
+    
+    for (int i = 3; i >= 0; i--)
+    {
+        printf("%i", bin[i]);
+    }
+    printf("\n");
 }
+
+int  sumbinaire(){
+	
+
+}
+
+
+int binToint(){
+	
+	
+	
+}
+
