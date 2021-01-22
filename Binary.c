3e6df869b7e239bade32b61039d6817d25393dcc
fatimajd <jdiouifatimazahera@gmail.com>
2021-01-22 11:13:51 +0100

    int a int b

--- Binary.c
+++ Binary.c
@@ -9,6 +9,16 @@ int main(){
 	
 }
 
-int  sumbinaire(int x ){
-		
+int  sumbinaire(int a , int b){
+		int s;
+		if(a==0 && b==1){
+			s=1;
+		}else 	if(a==1 && b==0){
+			s=1;
+		}else 	if(a==0 && b==0){
+			s=0;
+		}else 	if(a==1 && b==1){
+			s=10;
+		}
+		return s;
 	}
