99cbf830ae657072605358027bea2aabac4fe365
fatimajd <jdiouifatimazahera@gmail.com>
2021-01-22 11:24:00 +0100

    fatima

--- Binary.c
+++ Binary.c
@@ -9,16 +9,6 @@ int main(){
 	
 }
 
-int  sumbinaire(int a , int b){
-		int s;
-		if(a==0 && b==1){
-			s=1;
-		}else 	if(a==1 && b==0){
-			s=1;
-		}else 	if(a==0 && b==0){
-			s=0;
-		}else 	if(a==1 && b==1){
-			s=10;
-		}
-		return s;
+int  sumbinaire(){
+	
 	}
